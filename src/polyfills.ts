42274b71385acc1eeb864e1a6c2a40db0d463612
Giuliano <giulianodb@gmail.com>
2023-04-08 14:54:59 -0300

    Configurações angular

---
+++ src/polyfills.ts
@@ -0,0 +1,17 @@
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+import 'zone.js/dist/zone';  // Para o Angular com detecção de mudança automática e atualização de view em tempo real
+
+/***************************************************************************************************
+ * ZONE JS - Remova este bloco para desativar a detecção de mudança automática e atualização de view em tempo real
+ */
+
+// import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * CUSTOM POLYFILLS
+ */
+
+// Inclua seus próprios polyfills, conforme necessário.
